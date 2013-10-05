d97f28e1f288e5afe5ccfa9d4fd20cff4c863a98
Trung Ngo <ndtrung4419@gmail.com>
2013-10-05 13:03:03 +0700

    chin

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,10 @@
 from nose.tools import eq_
 
 def fizzbuzz(number):
+    if number == 1:
+        return "1"
     return "fizz"
 
 eq_("fizz", fizzbuzz(3))
 eq_("fizz", fizzbuzz(6))
+eq_("1", fizzbuzz(1))
