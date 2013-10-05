f6b86de59ca71ad8e756a281d864d2a5a04597ca
Trung Ngo <ndtrung4419@gmail.com>
2013-10-05 13:23:39 +0700

    tai

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,6 +4,8 @@ from nose.tools import eq_
 def fizzbuzz(number):
     if number == 1:
         return "1"
+    if number == 2:
+        return "2"
     elif number % 15 == 0:
         return "fizzbuzz"
     elif number % 5 == 0:
@@ -16,3 +18,4 @@ eq_("fizz", fizzbuzz(6))
 eq_("1", fizzbuzz(1))
 eq_("buzz", fizzbuzz(5))
 eq_("fizzbuzz", fizzbuzz(15))
+eq_("2", fizzbuzz(2))
